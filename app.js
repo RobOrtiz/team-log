78e4308419bf2a940b5e47b4482986fa8e7ed083
RobOrtiz <mrrobortiz@gmail.com>
2020-12-12 23:49:04 -0800

    updated app js file

--- app.js
+++ app.js
@@ -10,42 +10,166 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const devMembers = [];
-const idArrray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function buildTeam() {
-    function devManager() {
-        console.log("Time to build your team");
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'managerName',
-                message: 'What is your name?',
-              },
-              {
-                type: 'input',
-                name: 'managerId',
-                message: 'What is your ID?',
-              },
-              {
-                type: 'input',
-                name: 'managerEmail',
-                message: 'What is your email?',
-              },
-              {
-                type: 'input',
-                name: 'managerOfficeNum',
-                message: 'What is your office number?',
-              },
-        ])
-            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
-            devMembers.push(manager);
-            idArrray.push(answers.managerId);
-            devTeam();       
+async function start(){
+    console.log("Time to build your team!");
+    let myTeamHTML = "";
+    let teamSize;
+
+    await inquirer.prompt(
+        {
+            type: "number",
+            message: "How many people are in your team?",
+            name: "memberNumer"
+        }
+    )
+    .then((data) => {
+
+        // Number of team members placed in teamSize for scope purposes.
+        // 1 is added start from 1 rather than 0 for user understanding.
+        teamSize = data.memberNumber + 1;
+    });
+    
+    // If Team Size is 0, will end program
+    if (teamSize === 0){
+        console.log("I guess there is no one on your team...");
+        return;
     }
+    
+    // Loop begins to ask questions depending on the size of the team
+    for(i = 1; i < teamSize; i++){
+
+        // Global variables set
+        let name;
+        let id;
+        let title;
+        let email;
+
+        // Prompts user to answer the basic questions of the employee
+        await inquirer.prompt([ 
+            {
+                type: "input",
+                message: `What is employee (${i})'s name?`,
+                name: "name"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s id?`,
+                name: "id"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s Email?`,
+                name: "email"
+            },
+            {
+                type: "list",
+                message: `what the employee (${i})'s title?`,
+                name: "title",
+                choices: ["Engineer", "Intern", "Manager"]
+            }
+        ])
+        .then((data) => {
+
+            // Takes data from user and places value in global variables
+            name = data.name;
+            id = data.id;
+            title = data.title;
+            email = data.email;
+        });
+
+        // Switch Case depending on the title of the employee
+        switch (title){
+            case "Manager":
+
+                // ask user of Manager's Office Number
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Manager's Office Number?",
+                        name: "officeNum"
+                    }
+                ])
+                .then((data) => {
+
+                    // Create a new object with all avaiable user input data
+                    const manager = new Manager(name, id, email, data.officeNo);
+
+                    // Reads and places HTML from manager.html in teamMemever Variable
+                    teamMember = fs.readFileSync("templates/manager.html");
+
+                    // Uses eval() to pass template literals from html files.
+                    // Adds the string to the team HTML.
+                    myTeamHTML = myTeamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            // intern
+            case "Intern":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What school is your Intern attending?",
+                        name: "school"
+                    }
+                ])
+                .then((data) => {
+                    const intern = new Intern(name, id, email, data.school);
+                    teamMember = fs.readFileSync("templates/intern.html");
+                    myTeamHTML = myTeamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            // Engineer
+            case "Engineer":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Engineer's GitHub?",
+                        name: "github"
+                    }
+                ])
+                .then((data) => {
+                    const engineer = new Engineer(name, id, email, data.github);
+                    teamMember = fs.readFileSync("templates/engineer.html");
+                    myTeamHTML = myTeamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+        } 
+
+    } 
+
+    // Reads main.html and places html in a variable
+    const mainHTML = fs.readFileSync("templates/main.html");
+    
+    // Use eval to implement template literals in main.html and places teamHTML inside main template
+    myTeamHTML = eval('`'+ mainHTML +'`');
+
+    // write file to new team.html file
+    fs.writeFile("output/team.html", myTeamHTML, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("All Done!");
+      
+      });
+
+    // console.log(teamHTML);
 }
+
+
+start();
+
+            // const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
+            // devMembers.push(manager);
+            // idArrray.push(answers.managerId);
+            // devTeam();       
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
